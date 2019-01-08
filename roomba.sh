81f7787985d0793db1fd675c2480e5e75b51bc0a
Prince_Kiwii <kevinalb12@gmail.com>
2019-01-08 15:56:08 +0100

    Code Refactoring to improve error Messages and code readability. Removed unnecessary statements

--- roomba.sh
+++ roomba.sh
@@ -4,6 +4,8 @@ function errcho() {
     echo "$@" 1>&2;
 }
 
+
+
 function roombash_on_load() {
 
     echo "RoombaSH has loaded correctly"
@@ -16,47 +18,46 @@ function roombash_cleanup() {
         touch .cleanup
         return
     fi
+
     FILES=$(cat .cleanup)
-    echo "@CleanUp: Performing CleanUp on {"$FILES"}" 
-    mkdir "cleanup_storage"
+    echo "RoombaSH: Performing CleanUp on {"$FILES"}" 
+    mkdir "RoombaSH: Creating Folder \"cleanup_storage\""
+
     for FILE in $FILES; do
-        UNCLEAN=0
+        UNCLEAN=0   # This is a bad variable name..
         ALL_IN=0
+
         if [[ ${FILE:0:1} == "!" ]]; then
-            echo "IGNORE::$FILE"
+            echo "RoombaSH: Ignoring \"$FILE\""
             continue
         fi
         if [[ ${FILE:0:1} = "^" ]]; then
-            echo "UNCLEAN::$FILE"
-            FILE=${FILE#"^"}
-            echo "NEW FILE NAME: $FILE"
+            FILE=${FILE#"^"} # Remove '^'
+            echo "RoombaSH: \"$FILE\" is set to not be restored."
             let UNCLEAN=1
         fi
         if [[ ${FILE:0:1} = ">" ]]; then
-            echo "ALL_IN::$FILE"
-            FILE=${FILE#">"}
-            echo "NEW FILE NAME: $FILE"
+            FILE=${FILE#">"} # Remove '#'
+            echo "RoombaSH: \"$FILE\" is set for Deep Cleanup."
             let ALL_IN=1
         fi
+
         DIR=$(dirname "$FILE")
         echo "DIRECTORY::$DIR"
         if [ -f $FILE ]; then
-            echo "Saving File::"$FILE""
+            echo "RoombaSH: Cleaning up FILE: \"$FILE\"."
             echo "" >> $FILE
             echo "$DIR" >> $FILE
             if [ "$UNCLEAN" -eq 1 ]; then
-                echo "UNCLEAN::$FILE"
-                new_file_name=$(basename "$FILE")
-                file_ext="${new_file_name##*.}"
-                new_file_name="${new_file_name%.*}.$file_ext"
-                COMP_FILE_NAME="$DIR/^$new_file_name"
-                mv $FILE $COMP_FILE_NAME
-                mv $COMP_FILE_NAME "cleanup_storage/"
+                BASE_NAME=$(basename "$FILE")
+                NEW_NAME="$DIR/^$BASE_NAME"
+                mv $FILE $NEW_NAME
+                mv $NEW_NAME "cleanup_storage/"
             else
                 mv $FILE "cleanup_storage/"
             fi
         elif [ -d $FILE ]; then
-            echo "Removing Directory::"$FILE"";
+            echo "RoombaSH: Cleaning up DIRECTORY: \"$FILE\"";
             if [ "$ALL_IN" -eq 1 ]; then
                 INFO_FILE="$FILE/.cleanupinfo"
                 touch $INFO_FILE
@@ -69,7 +70,7 @@ function roombash_cleanup() {
                 rm -r $FILE
             fi
         else
-            echo "Can't find "$FILE""
+            errcho "RoombaSH: Couldn't find "$FILE""
         fi
     done
 }
@@ -77,23 +78,24 @@ function roombash_cleanup() {
 function roombash_restore() {
 
     for FILE in ./cleanup_storage/*; do
-        echo "Moving "$FILE" back."
-        file_name=$(basename "$FILE")
-        if [[ ${file_name:0:1} == "^" ]]; then
+        echo "RoombaSH: Restoring \"$FILE\"..."
+        FILE_NAME=$(basename "$FILE")
+        if [[ ${FILE_NAME:0:1} == "^" ]]; then
+            echo "RoombaSH: FILE \"$FILE\" is not set for Restoration. Ignoring..."
             continue
         fi
         if [[ -d $FILE ]]; then
             INFO_FILE="$FILE/.cleanupinfo"
             PREV_PATH=$(cat $INFO_FILE)
             if [ "${PREV_PATH:0:1}" == "^" ]; then
-                echo "$FILE is UNCLEAN. Not Restoring."
+                echo "RoombaSH: Directory \"$FILE\" is not set for Restoration. Ignoring..."
                 continue
             fi
             rm $INFO_FILE
             mv -v -f $FILE $PREV_PATH
             continue;
         fi
-        # We obtain the previous path from the file
+        # We obtain the previous path from the last line of the file
         PREV_PATH=$(tail -n1 $FILE)
         if [ ! -d $PREV_PATH ]; then
             echo "Directory::$PREV_PATH not found. Creating..."
